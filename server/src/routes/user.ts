60ca356276bcdd8a14f266bf92cc39bfe28cf8d2
Atharv Nandrukar <atharvnandurkar@gmail.com>
2025-08-03 04:46:33 +0000

    user signin ep done

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,7 +1,11 @@
+import dotenv from 'dotenv'
+dotenv.config();
 import express = require("express");
 const userRouter = express.Router();
 import { PrismaClient } from '@prisma/client'
 const client = new PrismaClient();
+import jwt from 'jsonwebtoken'
+const JWT_CODE = process.env.JWT_CODE as string;
 
 userRouter.post("/signup", async (req, res) => {
     const { name, password, email, username } = req.body;
@@ -27,5 +31,28 @@ userRouter.post("/signup", async (req, res) => {
     }
 })
 
+userRouter.post("/signin", async (req, res) => {
+    const { username, password} = req.body;
+    const findUser = await client.user.findFirst({
+        where: {
+            username: username,
+            password: password
+        }
+    })
+    if(findUser) {
+        console.log(findUser);
+        const token = jwt.sign({
+            id: findUser.id
+        }, JWT_CODE);
+        res.json({
+            token
+        })
+    } 
+    else {
+        res.json({
+            message: "Error occur"
+        })
+    }
+})
 
 export default userRouter;
